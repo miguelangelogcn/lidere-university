1c1e95d1547009f8a1cb74d0367b722ad4be1efd
Miguel Ângelo <miguelangelordantascn@gmail.com>
2025-07-07 13:10:12 +0000

    Tudo bem. Agora, preciso que você me instrua como atualizar a imagem que

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,17 +1,16 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <svg
-      className={cn("h-8 w-8 text-white", className)}
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M15.5 2.5a.5.5 0 0 1 .5.5v18a.5.5 0 0 1-1 0V3a.5.5 0 0 1 .5-.5Z" />
-      <path d="m16 3 5.914 5.914a.5.5 0 0 1-.353.854H16V3Z" />
-      <path d="M8.5 2.5a.5.5 0 0 0-.5.5v18a.5.5 0 0 0 1 0V3a.5.5 0 0 0-.5-.5Z" />
-      <path d="m8 3-5.914 5.914a.5.5 0 0 0 .353.854H8V3Z" />
-    </svg>
+    <div className={cn("relative", className)}>
+      <Image
+        src="https://placehold.co/100x100.png"
+        alt="Lidere University Logo"
+        fill
+        className="object-contain"
+        data-ai-hint="logo"
+      />
+    </div>
   );
 }
