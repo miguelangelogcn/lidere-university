22a4fab356f39de58b6f311a28ea87d04d83db6a
Miguel Ângelo <miguelangelordantascn@gmail.com>
2025-07-10 16:30:47 +0000

    Agora quando fui clicar na pagina de dashboard financeiro (/src/app/(app

--- src/lib/modules.ts
+++ src/lib/modules.ts
@@ -49,10 +49,10 @@ export const appModules: Module[] = [
     id: "diretoria",
     name: "Diretoria",
     items: [
- { href: "/src/app/(app)/dashboard-financeiro", icon: DollarSign, label: "Dashboard Financeiro" },
- { href: "/src/app/(app)/dashboard-vendas", icon: AreaChart, label: "Dashboard de Vendas" },
- { href: "/src/app/(app)/dashboard-operacoes", icon: ClipboardCheck, label: "Dashboard de Operações" },
- { href: "/src/app/(app)/dashboard-conteudos", icon: GraduationCap, label: "Dashboard de Conteúdos" },
+ { href: "/dashboard-financeiro", icon: DollarSign, label: "Dashboard Financeiro" },
+ { href: "/dashboard-vendas", icon: AreaChart, label: "Dashboard de Vendas" },
+ { href: "/dashboard-operacoes", icon: ClipboardCheck, label: "Dashboard de Operações" },
+ { href: "/dashboard-conteudos", icon: GraduationCap, label: "Dashboard de Conteúdos" },
     ]
   },
   {
