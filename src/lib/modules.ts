a791d2a73fc58d59677cc54fbc6a77cdd6a337ae
Miguel Ângelo <miguelangelordantascn@gmail.com>
2025-07-11 19:23:35 +0000

    O mesmo erro continua acontecendo. Recentemente as suas alterações não t

--- src/lib/modules.ts
+++ src/lib/modules.ts
@@ -53,7 +53,7 @@ export const appModules: Module[] = [
      { href: "/dashboard-vendas", icon: AreaChart, label: "Dashboard de Vendas" },
      { href: "/dashboard-operacoes", icon: ClipboardCheck, label: "Dashboard de Operações" },
      { href: "/dashboard-conteudos", icon: LayoutDashboard, label: "Dashboard de Conteúdos" },
-     { href: "/dashboard-financeiro", icon: LineChart, label: "Relatório Financeiro" },
+     { href: "/relatorio-financeiro", icon: LineChart, label: "Relatório Financeiro" },
     ]
   },
   {
