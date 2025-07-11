7f72038ea69f4f1d3aa63657433d5b3674003dd5
Miguel Ângelo <miguelangelordantascn@gmail.com>
2025-07-11 21:45:07 +0000

    O problema de instalação foi resolvido. O erro 404 final está sendo caus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,5 @@
-
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuth } from '@/context/auth-provider';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-  const { user, loading } = useAuth();
-
-  useEffect(() => {
-    if (!loading) {
-      if (user) {
-        router.replace('/inicio');
-      } else {
-        router.replace('/login');
-      }
-    }
-  }, [user, loading, router]);
-
-  return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <p>Carregando...</p>
-    </div>
-  );
+  redirect('/inicio');
 }
